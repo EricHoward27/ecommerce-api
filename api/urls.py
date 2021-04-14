6cb7b11789cfd8e96f1b8c9adbdaafcca3244e68
eric <erichoward2141@gmail.com>
2021-04-14 13:38:14 -0500

    add show, delete, and update request in views; check for superuser when creating product, add url with pk to route

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.mango_views import Mangos, MangoDetail
-from .views.product_views import ProductView
+from .views.product_views import ProductView, ProductViewDetail
 from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('mangos/', Mangos.as_view(), name='mangos'),
     path('mangos/<int:pk>/', MangoDetail.as_view(), name='mango_detail'),
     path('products/', ProductView.as_view(), name='products'),
+    path('products/<int:pk>/', ProductViewDetail.as_view(), name='product_detail'),
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
--- api/views/product_views.py
+++ api/views/product_views.py
@@ -6,15 +6,20 @@ from rest_framework import generics, status
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user, authenticate, login, logout
 from django.middleware.csrf import get_token
+from django.contrib.auth.mixin import UserPassesTestMixin
 
 from ..serializers import ProductSerializer, UserSerializer
 from ..models.product import Product
 
-class ProductView(generics.ListCreateAPIView):
+class SuperUserCheck(UserPassesTestMixin, APIView):
+    def is_superuser(self):
+        return self.request.user.is_superuser
+
+class ProductView(SuperUserCheck, generics.ListCreateAPIView):
     permission_classes=(IsAuthenticated,)
     serializer_class = ProductSerializer
     def get(self, request):
-        """Index request"""
+        """Index Product Request"""
         # Get all the mangos:
         # mangos = Mango.objects.all()
         # Filter the mangos by owner, so you can only see your owned mangos
@@ -22,3 +27,57 @@ class ProductView(generics.ListCreateAPIView):
         # Run the data through the serializer
         data = ProductSerializer(products, many=True).data
         return Response({ 'products': data })
+    
+    def post(self, request):
+        """Create Product Request"""
+        request.data['product']['owner'] = request.user.id
+        # Serialize/create product
+        product = ProductSerializer(data=request.data['product'])
+        if product.is_valid():
+            # Save the create product & send a response
+            product.save()
+            return Response({ 'products': product.data }, status=status.HTTP_201_CREATED)
+         # If the data is not valid, return a response with the errors
+        return Response(product.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=(IsAuthenticated,)
+    def get(self, request, pk):
+        """Show Product Request"""
+        # Find the product to show
+        product = get_object_or_404(Product, pk=pk)
+        # Only show the product that is owned
+        if not request.user.id == product.owner.id:
+            raise PermissionDenied('Unauthorized, you do not own this product')
+        
+        # Send the data through the serializer to be formatted
+        data = ProductSerializer(product).data
+        return Response({ 'product': data })
+    
+    def delete(self, request, pk):
+        """Delete Product Request"""
+        product = get_object_or_404(Product, pk=pk)
+        if not request.user.id == product.owner.id:
+            raise PermissionDenied('Unauthorized, you do not own this product')
+        product.delete()
+        return Response((status=status.HTTP_204_NO_CONTENT))
+    
+    def partial_update(self, request, pk):
+        """Update Product Request"""
+        # Remove the owner request object, get the owner key on the data dict and return flase if it doesn't find it. So if it is found remove it
+        if request.data['product'].get('owner', False):
+            del request.data['product']['owner']
+        
+        # Locate the product
+        product = get_object_or_404(Product, pk=pk)
+        if not request.user.id == product.owner.id:
+            raise PermissionDenied('Unauthorized, you do not own this product')
+        
+        # add owner to data no that we know this user owns the resource
+        request.data['product']['owner'] = request.user.id
+        # validate updates with serializer
+        data = ProductSerializer(product, data=request.data['product'])
+        if data.is_valid():
+            data.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
